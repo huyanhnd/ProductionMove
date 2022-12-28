2ce4589be2a023995856570d8b9b9c205baaf1e8
aiden.nguyen <aiden.nguyen@avepoint.com>
2022-12-28 21:47:45 +0700

    Update User

--- client/src/pages/admin/account/user/User.jsx
+++ client/src/pages/admin/account/user/User.jsx
@@ -1,25 +1,78 @@
 import {
   CalendarToday,
-  LocationSearching,
-  MailOutline,
   PermIdentity,
-  PhoneAndroid,
   Publish,
   Man
 } from "@mui/icons-material";
-import { useEffect } from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { getUsers } from "../../../../api/userApi";
+import { useLocation } from "react-router-dom";
+import { formatDate } from "../../../../helper/formatDate";
+import app from "../../../../firebase"
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import "./user.css";
+import { updateUser } from "../../../../api/userApi";
 
 export default function User() {
-  const dispatch = useDispatch();
-  const user = useSelector((state) => state.user.users);
+  const location = useLocation();
+  const userId = location.pathname.split("/")[2];
 
-  useEffect(() => {
-    getUsers(dispatch);
-  }, [dispatch]);
+  const dispatch = useDispatch();
+  const [inputs, setInputs] = useState({ role: "Factory" });
+  const [file, setFile] = useState(null);
+
+  const handleChange = (e) => {
+    setInputs(prev => {
+      return { ...prev, [e.target.name]: e.target.value }
+    })
+  }
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    const fileName = new Date().getTime() + file.name;
+    const storage = getStorage(app);
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    // Register three observers:
+    // 1. 'state_changed' observer, called any time the state changes
+    // 2. Error observer, called on failure
+    // 3. Completion observer, called on successful completion
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+          default:
+        }
+      },
+      (error) => {
+        // Handle unsuccessful uploads
+      },
+      () => {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          const user = { ...inputs, image: downloadURL };
+          updateUser(userId, user, dispatch);
+          alert("Update User Success!");
+        });
+      }
+    );
+  };
+
+  const user = useSelector((state) => state.user.users);
+  const selectUser = user.find(u => u.id == userId)
 
   return (
     <div className="user">
@@ -30,38 +83,28 @@ export default function User() {
         <div className="userShow">
           <div className="userShowTop">
             <img
-              src="https://firebasestorage.googleapis.com/v0/b/productionmove-3cd59.appspot.com/o/1671172307669roberto-nickson-Gvm2wM3V5PA-unsplash.jpg?alt=media&token=4e9bcb67-6160-4074-9850-ce734b3c6a92"
+              src={selectUser.image}
               alt=""
               className="userShowImg"
             />
             <div className="userShowTopTitle">
-              {/* <span className="userShowUsername">{user.fullname}</span> */}
-              <span className="userShowUserTitle">Fullname</span>
+              <span className="userShowUserTitle">{selectUser.fullName}</span>
             </div>
           </div>
           <div className="userShowBottom">
             <span className="userShowTitle">Account Details</span>
             <div className="userShowInfo">
               <PermIdentity className="userShowIcon" />
-              <span className="userShowInfoTitle">username</span>
+              <span className="userShowInfoTitle">{selectUser.username}</span>
             </div>
             <div className="userShowInfo">
               <CalendarToday className="userShowIcon" />
-              <span className="userShowInfoTitle">created</span>
+              <span className="userShowInfoTitle">{formatDate(selectUser.created)}</span>
             </div>
-            {/* <span className="userShowTitle">Description Details</span> */}
             <div className="userShowInfo">
               <Man className="userShowIcon" />
-              <span className="userShowInfoTitle">Role</span>
+              <span className="userShowInfoTitle">{selectUser.role}</span>
             </div>
-            {/* <div className="userShowInfo">
-              <MailOutline className="userShowIcon" />
-              <span className="userShowInfoTitle">annabeck99@gmail.com</span>
-            </div>
-            <div className="userShowInfo">
-              <LocationSearching className="userShowIcon" />
-              <span className="userShowInfoTitle">New York | USA</span>
-            </div> */}
           </div>
         </div>
         <div className="userUpdate">
@@ -74,6 +117,7 @@ export default function User() {
                   type="text"
                   placeholder="username"
                   className="userUpdateInput"
+                  onChange={handleChange}
                 />
               </div>
               <div className="userUpdateItem">
@@ -82,47 +126,36 @@ export default function User() {
                   type="text"
                   placeholder="fullname"
                   className="userUpdateInput"
-                />
-              </div>
-              <div className="userUpdateItem">
-                <label>Email</label>
-                <input
-                  type="text"
-                  placeholder="annabeck99@gmail.com"
-                  className="userUpdateInput"
+                  onChange={handleChange}
                 />
               </div>
               <div className="userUpdateItem">
                 <label>Role</label>
-                <select className="selectRole">
-                  <option value="manufacture">Admin</option>
-                  <option value="store">Factory</option>
-                  <option value="sold">Store</option>
-                  <option value="error">ServiceCenter</option>
+                <select className="selectRole" onChange={handleChange}>
+                  <option value="Factory">Factory</option>
+                  <option value="Store">Store</option>
+                  <option value="ServiceCenter">ServiceCenter</option>
                 </select>
               </div>
-              {/* <div className="userUpdateItem">
-                <label>Address</label>
-                <input
-                  type="text"
-                  placeholder="New York | USA"
-                  className="userUpdateInput"
-                />
-              </div> */}
             </div>
             <div className="userUpdateRight">
               <div className="userUpdateUpload">
                 <img
                   className="userUpdateImg"
-                  src="https://firebasestorage.googleapis.com/v0/b/productionmove-3cd59.appspot.com/o/1671172307669roberto-nickson-Gvm2wM3V5PA-unsplash.jpg?alt=media&token=4e9bcb67-6160-4074-9850-ce734b3c6a92"
+                  src={selectUser.image}
                   alt=""
+                  onChange={(e) => {
+                    if (file) {
+                      console.log(file)
+                    }
+                  }}
                 />
                 <label htmlFor="file">
                   <Publish className="userUpdateIcon" />
                 </label>
-                <input type="file" id="file" style={{ display: "none" }} />
+                <input type="file" id="file" style={{ display: "none" }} onChange={(e) => setFile(e.target.files[0])} />
               </div>
-              <button className="userUpdateButton">Update</button>
+              <button className="userUpdateButton" onClick={handleClick}>Update</button>
             </div>
           </form>
         </div>
