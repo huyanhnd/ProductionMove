3ebb9a30761947e9f2a095f4ebaff71cbee13be2
aiden.nguyen <aiden.nguyen@avepoint.com>
2022-12-28 08:54:43 +0700

    export

--- client/src/pages/factory/export/newExport/NewExport.jsx
+++ client/src/pages/factory/export/newExport/NewExport.jsx
@@ -1,52 +1,42 @@
 import { useState } from "react";
 import "./newExport.scss";
 import { useDispatch } from "react-redux";
+import AddFactoryProduct from "../../warehouse/AddFactoryProduct";
 
 export default function NewExport() {
-  const dispatch = useDispatch();
-  const [inputs, setInputs] = useState({role: "Factory"});
-  const [file, setFile] = useState(null);
-
-  const handleChange = (e) => {
-    setInputs(prev => {
-      return { ...prev, [e.target.name]: e.target.value }
-    })
-  }
-
   return (
-    <div className="newUser">
-      <h1 className="newUserTitle">New User</h1>
-      <form className="newUserForm">
-        <div className="newUserItem">
-          <label>Name</label>
-          <input type="text" placeholder="john" name="username" onChange={handleChange} />
+    <div className="newProduct">
+      <h1 className="addProductTitle">New Export</h1>
+      <form className="addProductForm">
+        <div className="productFlex">
+          <div className="nameProduct">
+            <span className="addTitle">New Export Request To Store</span>
+            <div className="productLine-text">
+              <div className="addProductItem">
+                <label>Name</label>
+                <input type="text" placeholder="Name ProductLine" />
+              </div>
+              <div className="addProductItem">
+                <label>To Store</label>
+                <select name="active" id="active">
+                  <option value="">iP11</option>
+                  <option value="">iP11PM</option>
+                  <option value="">iP12</option>
+                  <option value="">iP12P</option>
+                  <option value="">iP12PM</option>
+                  <option value="">iP13</option>
+                  <option value="">iP13P</option>
+                  <option value="">iP13PM</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div className="detailProduct">
+            <AddFactoryProduct />
+          </div>
         </div>
-        <div className="newUserItem">
-          <label>Full Name</label>
-          <input type="text" placeholder="John Smith" name="fullName" onChange={handleChange} />
-        </div>
-        <div className="newUserItem">
-          <label>Role</label>
-          <select className="newUserSelect" name="role" id="role" onChange={handleChange} >
-            <option value="Factory">Factory</option>
-            <option value="store">Store</option>
-            <option value="serviceCenter">ServiceCenter</option>
-          </select>
-        </div>
-        <div className="newUserItem">
-          <label>Password</label>
-          <input type="password" name="password" placeholder="********" onChange={handleChange} />
-        </div>
-        <div className="newUserItem">
-          <label>Confirm PassWord</label>
-          <input type="resetpassword" name="confirmPassword" placeholder="********" onChange={handleChange} />
-        </div>
-        <div className="newUserItem">
-          <label>Image</label>
-          <input type="file" onChange={(e) => setFile(e.target.files[0])} />
-        </div>
-        <button className="newUserButton">Create</button>
-      </form >
-    </div >
+        <button className="addProductButton">Create</button>
+      </form>
+    </div>
   );
 }
